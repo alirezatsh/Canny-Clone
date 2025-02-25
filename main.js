4a0e416d3ccb386d200fb6e3214fc76ff84b5866
alirezatsh <alirezatashakkori43@gmail.com>
2025-02-25 09:57:31 +0330

    Config mongdb connections

--- main.js
+++ main.js
@@ -3,17 +3,18 @@ const express = require('express')
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./src/swagger.json");
 const errorHandler = require('./src/middlewares/errorhandlers');
+const connectToDb = require('./src/config/db')
+
 
 const app = express()
 const port = process.env.PORT || 3000;
 
 
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-
-
 app.use(express.json())
 app.use(errorHandler); 
+
+connectToDb()
 
 
 
---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const dbOptions = {
+    maxPoolSize: 10, // Set the pool size as needed
+  };
+
+const connectToDb = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGODB_URI , dbOptions)
+        console.log('mongodb is connected successfully :)')
+    }
+    
+    catch(e){
+        console.log('mongo connection failed' , e)
+        process.exit(1)
+    }
+
+}
+
+module.exports = connectToDb
